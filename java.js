65684f73d81e1e40af3c0b9201087661195d5fd3
fths20354 <up6xu6m4cj3@gmail.com>
2019-10-01 20:03:16 +0800

    JS

--- java.js
+++ java.js
@@ -1,6 +1,9 @@
 var toki = new Vue({
     el : "#time",
     data : {
+        year : "",
+        month : "",
+        date : "",
         hour : "",
         minute : "",
         second : ""
@@ -8,6 +11,9 @@ var toki = new Vue({
     methods: {
         setClock(){
             var date = new Date();
+            this.year = date.getFullYear();
+            this.month = date.getMonth();
+            this.date = date.getDate();
             this.hour = date.getHours();
             this.minute = date.getMinutes();
             this.second = date.getSeconds();
